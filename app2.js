0f1e325a96ab6a70376a0b53439c7768be15450b
xieerduos <1454598684@qq.com>
2018-03-15 17:48:39 +0800

    vue事件

--- app2.js
+++ app2.js
@@ -3,7 +3,9 @@
 new Vue({
     el: "#vue-app",
     data: {
-        age: 30
+        age: 30,
+        x:0,
+        y:0
     },
     methods: {
         add: function(inc) {
@@ -11,6 +13,10 @@ new Vue({
         },
         subtract: function(dec) {
             this.age -= dec;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
